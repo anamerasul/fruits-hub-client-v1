b918b056668c786facec3bdc68e3da45c965f150
anamerasul <anamerasul@gmail.com>
2022-05-07 02:22:24 +0600

    spinner add

--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { Link } from "react-router-dom";
 import auth from "../../FirebaseConfig/Firebase.init";
+import Spinner from "../Shared/Spinner/Spinner";
 
 import "./NotFound.module.css";
 
@@ -9,10 +10,10 @@ const NotFound = () => {
   const [user] = useAuthState(auth);
   return (
     <div
-      className="notFound bg-yellow-200 flex justify-around flex-col md:flex-row h-[90vh]"
+      className="notFound bg-yellow-300 flex justify-around items-between flex-col md:flex-row h-[90vh]"
       style={{
         backgroundImage: "url(https://i.ibb.co/4Zbryfc/notfoundpage-1.png)",
-        backgroundSize: "cover",
+        backgroundSize: "80% 100%",
         backgroundRepeat: "no-repeat",
       }}
     >
@@ -24,32 +25,40 @@ const NotFound = () => {
         />
       </div> */}
 
-      <div className="absoulate my-12 flex flex-col">
-        {user ? (
-          <h3 class="text-red-900 text-4xl font-black capitalize">
-            Your Page is not found
-          </h3>
-        ) : (
-          <h3 class="text-red-900 text-4xl font-black capitalize">
-            page not found <span>Login to access this page</span>
-          </h3>
-        )}
+      <div className=" my-12 flex flex-col items-between justify-between">
+        <div>
+          {user ? (
+            <h3 class="text-red-900 bg-yellow-400 md:bg-transparent p-4 text-xl md:text-4xl  font-black capitalize">
+              Page not found
+            </h3>
+          ) : (
+            <h3 class="text-red-900 bg-yellow-400 md:bg-transparent p-4 text-xl md:text-4xl  font-black capitalize">
+              page not found <span>Login to access this page</span>
+            </h3>
+          )}
+        </div>
 
-        {user ? (
-          <Link
-            to="/"
-            className="w-half px-6 py-2 mt-4 text-white bg-orange-600 rounded-md hover:bg-orange-800"
-          >
-            Back to home
-          </Link>
-        ) : (
-          <Link
-            to="/Login"
-            className="w-half px-6 py-2 mt-4 text-white bg-orange-600 rounded-md hover:bg-orange-800"
-          >
-            Login
-          </Link>
-        )}
+        <div className="my-5">
+          <Spinner></Spinner>
+        </div>
+
+        <div>
+          {user ? (
+            <Link
+              to="/"
+              className=" w-full px-6 py-2 mt-4 text-white bg-orange-600 rounded-md hover:bg-orange-800"
+            >
+              Back to home
+            </Link>
+          ) : (
+            <Link
+              to="/Login"
+              className=" w-full px-6 py-2 mt-4 text-white bg-orange-600 rounded-md hover:bg-orange-800"
+            >
+              Login
+            </Link>
+          )}
+        </div>
       </div>
     </div>
   );
