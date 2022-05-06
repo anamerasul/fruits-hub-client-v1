ab99039f4c0b25c262fa4eae5525f43cbc024d18
anamerasul <anamerasul@gmail.com>
2022-05-07 03:54:47 +0600

    swipper add

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,7 +1,72 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css/bundle";
+import "swiper/css";
+import "./banner.css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Pagination, Navigation } from "swiper";
+import { Link } from "react-router-dom";
 
 const Banner = () => {
-  return <div>Banner</div>;
+  const array = [1, 3, 5, 6];
+  return (
+    <div className="relative">
+      <div className="h-[80vh] w-[100vw] bg-yellow-200 ">
+        <Swiper className="mySwiper">
+          {/* <SwiperSlide className="bg-yellow-300">
+            <img
+              className="w-25 h-25 bg-yellow-300"
+              src="https://i.ibb.co/4Zbryfc/notfoundpage-1.png"
+              alt=""
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              className="w-25 h-25 bg-yellow-300"
+              src="https://i.ibb.co/4Zbryfc/notfoundpage-1.png"
+              alt=""
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              className="w-25 h-25 bg-yellow-300"
+              src="https://i.ibb.co/4Zbryfc/notfoundpage-1.png"
+              alt=""
+            />
+          </SwiperSlide> */}
+
+          {array.map((arr) => (
+            <SwiperSlide className="bg-yellow-300">
+              <img
+                className="w-25 h-25 bg-yellow-300"
+                src="https://i.ibb.co/4Zbryfc/notfoundpage-1.png"
+                alt=""
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+
+      <div className="h-[60vh] sm:h-[30vh] absolute top-5 left-10 mt-10 sm:top-16 sm:left-20 z-50 sm:mt-24 flex flex-col justify-between items-between">
+        <div>
+          <h2 className="text-4xl sm:text-6xl font-black text-green-700">
+            Welcome to fruits hub
+          </h2>
+        </div>
+
+        <div>
+          <Link
+            to="/manageinventory"
+            className=" w-full px-6 py-2 mt-4 text-white bg-orange-600 rounded-md hover:bg-orange-800"
+          >
+            Manage inventory
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
--- src/Pages/Home/HomeInventory/HomeInventory.js
+++ src/Pages/Home/HomeInventory/HomeInventory.js
@@ -1,7 +1,23 @@
 import React from "react";
 
 const HomeInventory = () => {
-  return <div>homeInventory</div>;
+  return (
+    <div>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora
+        distinctio repudiandae autem tenetur, corporis veritatis? Dolore veniam
+        accusamus sunt deleniti deserunt, modi maxime eius impedit asperiores
+        suscipit debitis harum delectus a sapiente nihil consequatur,
+        reprehenderit necessitatibus enim provident expedita ipsam! Illo quos
+        earum vitae dolor quibusdam explicabo, beatae facere doloremque nisi eum
+        suscipit non cumque voluptates blanditiis sit ab culpa omnis et odio
+        eius, deserunt sapiente illum? Quia id rem repellat eligendi ab? Minima
+        maiores ab pariatur repellendus, minus amet reiciendis tenetur earum
+        nam! Dolores maiores expedita magnam quia nobis, libero enim illo,
+        itaque cupiditate vel iste exercitationem voluptatibus. Dolores!
+      </p>
+    </div>
+  );
 };
 
 export default HomeInventory;
