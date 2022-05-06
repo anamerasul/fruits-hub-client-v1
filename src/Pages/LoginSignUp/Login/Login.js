d62664fbf0480ed944b47aaa6595728eb0a2a59a
anamerasul <anamerasul@gmail.com>
2022-05-06 20:42:01 +0600

    login implement

--- src/Pages/LoginSignUp/Login/Login.js
+++ src/Pages/LoginSignUp/Login/Login.js
@@ -1,15 +1,56 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import {
+  useAuthState,
+  useSignInWithEmailAndPassword,
+} from "react-firebase-hooks/auth";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import auth from "../../../FirebaseConfig/Firebase.init";
 import SocialLogin from "../SocialLogin/SocialLogin";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [signInWithEmailAndPassword, user, loading, error] =
+    useSignInWithEmailAndPassword(auth);
+  const navigate = useNavigate();
+  const location = useLocation();
+  const from = location.state?.form?.pathname || "/";
+
+  const [authuser] = useAuthState(auth);
+
+  const handleLoginSubmit = (e) => {
+    e.preventDefault();
+
+    signInWithEmailAndPassword(email, password);
+  };
+
+  useEffect(() => {
+    if (user) {
+      navigate(from, { replace: true });
+    }
+  }, []);
+
+  if (error) {
+    return (
+      <div>
+        <p>Error: {error.message}</p>
+      </div>
+    );
+  }
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+  if (authuser) {
+    navigate(from, { replace: true });
+  }
+
   return (
     <div className="flex items-center justify-center">
       <div className="px-8 py-6 mx-4 mt-4 text-left bg-white md:w-1/3 lg:w-1/3 sm:w-1/3">
         <h3 className="text-2xl font-bold text-center">
           Login to your Account
         </h3>
-        <form action="">
+        <form onSubmit={handleLoginSubmit} action="">
           <div className="mt-4">
             <div></div>
             <div className="mt-4">
@@ -17,7 +58,9 @@ const Login = () => {
                 Email
               </label>
               <input
-                type="text"
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
                 placeholder="Email"
                 name="email"
                 className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"
@@ -27,6 +70,8 @@ const Login = () => {
               <label className="block">Password</label>
               <input
                 type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
                 name="password"
                 placeholder="Password"
                 className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"
