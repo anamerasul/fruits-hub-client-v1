8691da1c925e29dee25f4d4b471668f4b7436389
anamerasul <anamerasul@gmail.com>
2022-05-09 17:17:38 +0600

    REchart install

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Banner from "../Banner/Banner";
 import HomeInventory from "../HomeInventory/HomeInventory";
+import StockVsDeliverdChart from "../StockVSDeliverdChart/StockVsDeliverdChart";
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
       <HomeInventory></HomeInventory>
+      <StockVsDeliverdChart></StockVsDeliverdChart>
     </div>
   );
 };
---
+++ src/Pages/Home/StockVSDeliverdChart/StockVsDeliverdChart.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const StockVsDeliverdChart = () => {
+  return (
+    <div>
+      <h1>Stock vs DEliverd chart</h1>
+    </div>
+  );
+};
+
+export default StockVsDeliverdChart;
